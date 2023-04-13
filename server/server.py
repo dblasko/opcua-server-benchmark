18931d2f12639cd7b3b4e255b2f667f077792d47
Grégoire de Lambertye <gdelambertye03@gmail.com>
2023-04-13 11:53:13 +0200

    simplify client & server + run R/W/Methaod call 1000 times

--- server/server.py
+++ server/server.py
@@ -1,100 +1,45 @@
 import asyncio
 import logging
-from pathlib import Path
 
-from asyncua import Server, ua, uamethod
-
-
-@uamethod
-def say_hello_xml(parent, happy):
-    print("Calling say_hello_xml")
-    if happy:
-        result = "I'm happy"
-    else:
-        result = "I'm not happy"
-    print(result)
-    return result
+from asyncua import Server, ua
+from asyncua.common.methods import uamethod
 
 
 @uamethod
-def say_hello(parent, happy):
-    if happy:
-        result = "I'm happy"
-    else:
-        result = "I'm not happy"
-    print(result)
-    return result
-
-
-@uamethod
-def say_hello_array(parent, happy):
-    if happy:
-        result = "I'm happy"
-    else:
-        result = "I'm not happy"
-    print(result)
-    return [result, "Actually I am"]
-
-
-class HelloServer:
-    def __init__(self, endpoint, name, model_filepath):
-        self.server = Server()
-        self.model_filepath = model_filepath
-        self.server.set_server_name(name)
-        self.server.set_endpoint(endpoint)
-
-    async def init(self):
-        await self.server.init()
-
-        #  This need to be imported at the start or else it will overwrite the data
-        await self.server.import_xml(self.model_filepath)
-
-        objects = self.server.nodes.objects
-
-        freeopcua_namespace = await self.server.get_namespace_index(
-            "urn:freeopcua:python:server"
-        )
-        hellower = await objects.get_child("0:Hellower")
-        hellower_say_hello = await hellower.get_child("0:SayHello")
-
-        self.server.link_method(hellower_say_hello, say_hello_xml)
-
-        await hellower.add_method(
-            freeopcua_namespace,
-            "SayHello2",
-            say_hello,
-            [ua.VariantType.Boolean],
-            [ua.VariantType.String],
-        )
-
-        await hellower.add_method(
-            freeopcua_namespace,
-            "SayHelloArray",
-            say_hello_array,
-            [ua.VariantType.Boolean],
-            [ua.VariantType.String],
-        )
-
-    async def __aenter__(self):
-        await self.init()
-        await self.server.start()
-        return self.server
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.server.stop()
+def func(parent, value):
+    return value * 2
 
 
 async def main():
-    script_dir = Path(__file__).parent.parent
-    async with HelloServer(
-        "opc.tcp://0.0.0.0:4840/freeopcua/server/",
-        "FreeOpcUa Example Server",
-        script_dir / "test_saying.xml",
-    ) as server:
+    _logger = logging.getLogger(__name__)
+    # setup our server
+    server = Server()
+    await server.init()
+    server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+
+    # set up our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = await server.register_namespace(uri)
+
+    # populating our address space
+    # server.nodes, contains links to very common nodes like objects and root
+    myobj = await server.nodes.objects.add_object(idx, "MyObject")
+    myvar = await myobj.add_variable(idx, "MyVariable", 6.7)
+    myReadOnlyvar = await myobj.add_variable(idx, "MyReadOnlyVariable", 7)
+    # Set MyVariable to be writable by clients
+    await myvar.set_writable()
+    await server.nodes.objects.add_method(
+        ua.NodeId("ServerMethod", idx),
+        ua.QualifiedName("ServerMethod", idx),
+        func,
+        [ua.VariantType.Int64],
+        [ua.VariantType.Int64],
+    )
+    _logger.info("Starting server!")
+    async with server:
         while True:
             await asyncio.sleep(1)
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(main(), debug=True)
