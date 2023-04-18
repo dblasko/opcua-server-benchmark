e7913c71e2a84adc2aec03ce4c0f1406693bfa7b
dblasko <daniel.blasko.dev@gmail.com>
2023-04-18 15:55:28 +0200

    Refactor experiment to only gather & output raw data

--- expertiments/clients/responsiveness-jitter-throughput.py
+++ expertiments/clients/responsiveness-jitter-throughput.py
@@ -1,21 +1,34 @@
 import asyncio
 import random
 import time
+from datetime import datetime
+from pathlib import Path
 
 import numpy as np
+import pandas as pd
 from asyncua import Client
 from tqdm import tqdm
 
 
-class ResponsivenessJitterExperiment:
-    def __init__(self, server_url, node_id, num_requests=1000, data_size=64):
+class ResponsivenessJitterThroughputExperiment:
+    """Experiment for measuring the responsiveness, jitter and throughput of an OPC UA server."""
+
+    def __init__(
+        self,
+        server_url,
+        node_id,
+        experiment_name=f'responsiveness_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}',
+        num_requests=1000,
+        data_size=64,
+    ):
         self.server_url = server_url
         self.node_id = node_id
+        self.experiment_name = experiment_name
         self.num_requests = num_requests
         self.data_size = data_size
 
-    async def measure_responsiveness(self, client, mode):
-        """Measures the responsiveness of a read or write request.
+    async def measure_response_times(self, client, mode):
+        """Measures the response time of a read or write request.
 
         Args:
             client: opcua client
@@ -25,7 +38,8 @@ class ResponsivenessJitterExperiment:
             ValueError: if mode is not "read" or "write"
 
         Returns:
-            float: responsiveness
+            float: start time of the request
+            float: end time of the request
         """
         node = client.get_node(self.node_id)  # Get the node object from the node ID
         # Create a random data value of the given size
@@ -40,10 +54,10 @@ class ResponsivenessJitterExperiment:
             raise ValueError("Invalid mode")
         end_time = time.time()
 
-        return end_time - start_time  # return the responsiveness
+        return (start_time, end_time)
 
     async def run_experiment(self, mode):
-        """Runs the experiment and computes the mean responsiveness and jitter.
+        """Runs the experiment and measures start- and end-times of requests.
 
         Args:
             mode: "read" or "write"
@@ -51,29 +65,39 @@ class ResponsivenessJitterExperiment:
         client = Client(self.server_url)
         await client.connect()
 
-        responsiveness_list = []
+        measurements = []
         for i in tqdm(
             range(self.num_requests),
-            desc=f"Running {mode} mode responsiveness/jitter experiment",
+            desc=f"Running {mode} mode responsiveness/jitter/throughput experiment",
             unit=" requests",
         ):
-            responsiveness = await self.measure_responsiveness(client, mode)
-            responsiveness_list.append(responsiveness)
-            # print(
-            #    f"Request {i+1}/{self.num_requests} completed with responsiveness {responsiveness:.3f} seconds"
-            # )
-
+            start_time, end_time = await self.measure_response_times(client, mode)
+            measurements.append(
+                {
+                    "start_time": start_time,
+                    "end_time": end_time,
+                    "data_size": self.data_size,
+                    "mode": mode,
+                }
+            )
         await client.disconnect()
 
         # Computing our experiment metrics
-        mean_responsiveness = np.mean(responsiveness_list)
-        jitter = np.std(responsiveness_list)
+        # mean_responsiveness = np.mean(responsiveness_list)
+        # jitter = np.std(responsiveness_list)
         # Print the results
-        print(f"\t➡️ Mean responsiveness: {mean_responsiveness:.3f} seconds")
-        print(f"\t➡️ Jitter: {jitter:.3f} seconds")
-        # TODO: write to file
+        # print(f"\t➡️ Mean responsiveness: {mean_responsiveness:.3f} seconds")
+        # print(f"\t➡️ Jitter: {jitter:.3f} seconds")
+        # Format and store the raw experiment results
+        df = pd.DataFrame().from_records(measurements)
+        output_file = f"response_times_{mode}.csv"
+        output_dir = Path(f"data/{self.experiment_name}")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        df.to_csv(output_dir / output_file, index=False)
+        print(f"\t➡️ Measurements written to {str(output_dir / output_file)}")
 
     async def list_server_objects(self):
+        """For debug purposes only. Lists all objects in the server to understand their id's/structure."""
         client = Client(self.server_url)
         await client.connect()
         root = client.get_objects_node()
@@ -92,11 +116,13 @@ class ResponsivenessJitterExperiment:
 
 
 if __name__ == "__main__":
+    # Example use
+
     # Define the server URL and the node ID of the data point
     server_url = "opc.tcp://localhost:4840"
     node_id = "ns=2;i=2"
     # Create an instance of the experiment
-    experiment = ResponsivenessJitterExperiment(server_url, node_id)
+    experiment = ResponsivenessJitterThroughputExperiment(server_url, node_id)
     # asyncio.run(experiment.list_server_objects())
     # Run the experiment with read mode
     asyncio.run(experiment.run_experiment("read"))
