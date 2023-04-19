089d04af2812870eab458fd29a67001d252653eb
dblasko <daniel.blasko.dev@gmail.com>
2023-04-19 15:02:29 +0200

    Add setup.py for dependency managemlent

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opcua-server-benchmark",
+    version="0.1",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "tqdm",
+        "pyaml",
+        "asyncua",
+        "click",
+    ],
+    packages=find_packages(),
+)
